2b4ccc9dd9e5ec358afa7ce4a0dad3abc65d77ac
gimgyuyeon <8080claire@naver.com>
2022-06-28 16:08:39 +0900

    gogo

--- Python/백트래킹/15649.c
+++ Python/백트래킹/15649.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void func(int k)
 {
     int n, m;
     int arr[10];
+    bool isused[10];
     
     if (k == m)
+    {
+        for(int i = 0; i < m; i++)
+            printf("%d ", *arr);
+        printf("\n");
+        return;
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        if(!isused[i])
+        {
+            arr[k] = i;
+            isused[i] = 1;
+            func(k+1);
+            isused[i] = 0;
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    func(0);
+    return 0;
 }
