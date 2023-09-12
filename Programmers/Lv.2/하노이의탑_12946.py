37597034ddb20bb89bfef01a3988ed5cd9fd5795
gimgyuyeon <8080claire@naver.com>
2023-09-12 15:43:57 +0900

    [PRG]Lv.2_하노이의탑_12946

---
+++ Programmers/Lv.2/하노이의탑_12946.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+def recursive(start, end, another, n, answer):
+    if n == 1:
+        answer.append([start, end])
+        return
+    
+    # 1. 1번에서 2번으로 n-1개를 이동
+    recursive(start, another, end, n-1, answer)
+    # 2. 1번에서 3번으로 이동
+    answer.append([start, end])
+    # 3. 2번에서 3번으로 n-1개를 이동
+    recursive(another, end, start, n-1, answer)
+
+def solution(n):
+    answer = []
+    recursive(1, 3, 2, n, answer)
+    return answer
+
+print(solution(n))
