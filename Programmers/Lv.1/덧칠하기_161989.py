d0369cf2ddfa785f1569e4453bcd215828abadcc
gimgyuyeon <8080claire@naver.com>
2023-09-29 23:59:28 +0900

    [PRG]Lv.1_덧칠하기_161989

---
+++ Programmers/Lv.1/덧칠하기_161989.py
@@ -0,0 +1,9 @@
+def solution(n, m, section):
+    answer = 1 # 칠하는 횟수
+    paint = section[0] # 덧칠 시작점
+    for i in range(1, len(section)):
+        if section[i] - paint >= m:
+            answer += 1
+            paint = section[i]
+            
+    return answer
