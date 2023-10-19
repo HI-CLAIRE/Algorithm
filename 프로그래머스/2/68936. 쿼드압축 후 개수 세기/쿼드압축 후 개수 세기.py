18e7bf35ac5d83abb85784550dec570bfd39a6d7
Claire Kim <88919138+HI-CLAIRE@users.noreply.github.com>
2023-10-19 13:41:30 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 98.07 ms, Memory: 12 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -0,0 +1,21 @@
+# 1. 큰 사각형 좌측상단 꼭지점과 내용물이 동일한가?
+# 2. 동일하지 않다면 l//2 해서 다시 1
+# 3. 동일하면 +1
+
+def recur(a, b, l, arr, ans):
+    start = arr[a][b]
+    for i in range(a,a+l):
+        for j in range(b,b+l):
+            if arr[i][j] != start:
+                l //= 2
+                recur(a,b,l,arr,ans)
+                recur(a+l,b,l,arr,ans)
+                recur(a,b+l,l,arr,ans)
+                recur(a+l,b+l,l,arr,ans)
+                return
+    ans[start] += 1
+
+def solution(arr):
+    ans = [0, 0]
+    recur(0, 0, len(arr), arr, ans)
+    return ans
