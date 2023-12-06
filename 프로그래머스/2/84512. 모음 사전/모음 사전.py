0e155620a6074b961c62b282644e97b18153da0a
Claire Kim <88919138+HI-CLAIRE@users.noreply.github.com>
2023-12-06 22:25:57 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,11 +1,11 @@
-from itertools import product
-
 def solution(word):
-    alpha = "AEIOU"
-    perms = []
-    for i in range(1, 6):
-        for p in product(alpha, repeat=i):
-            perms.append(''.join(p))
-    perms.sort()
-    ans = perms.index(word) + 1
-    return ans
+    answer = 0
+    dic = ['A', 'E', 'I', 'O', 'U']
+    li = [5**i for i in range(len(dic))]
+    
+    for i in range(len(word)-1,-1,-1):
+        idx = dic.index(word[i])
+        for j in range(5-i):
+            answer += li[j]*idx
+        answer+=1
+    return answer
