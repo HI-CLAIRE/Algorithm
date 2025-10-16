48ce196ad16ece4a32a941479e0e630aa90368e2
Claire Kim <88919138+HI-CLAIRE@users.noreply.github.com>
2025-10-16 12:08:52 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,8 +1,5 @@
 function solution(a, b) {
-    const tmp1 = a.toString() + b.toString();
-    const res1 = parseInt(tmp1);
-    const tmp2 = b.toString() + a.toString();
-    const res2 = parseInt(tmp2);
-    
-    return res1 >= res2 ? res1 : res2
+  const num1 = Number(`${a}${b}`);
+  const num2 = Number(`${b}${a}`);
+  return Math.max(num1, num2);
 }
